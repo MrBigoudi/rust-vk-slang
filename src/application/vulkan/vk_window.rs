e8722ef5afd44682e7bd8aac84864af7d88577bc
MrBigoudi <yannis.kedadry2@gmail.com>
2024-07-10 15:11:41 +0200

    center window

--- src/application/vulkan/vk_window.rs
+++ src/application/vulkan/vk_window.rs
@@ -1,8 +1,7 @@
 use crate::application::vk_app::{AppParameters, VulkanApp};
 
 use winit::{
-    event_loop::EventLoop,
-    window::{Window, WindowBuilder},
+    dpi::{LogicalSize, PhysicalSize, Position}, event_loop::EventLoop, window::{Window, WindowBuilder}
 };
 
 impl VulkanApp {
@@ -14,13 +13,32 @@ impl VulkanApp {
 
     /// Initializes the window with given application parameters and event loop.
     pub fn init_window(app_params: &AppParameters, event_loop: &EventLoop<()>) -> Window {
-        WindowBuilder::new()
+        let primary_monitor = event_loop.primary_monitor().unwrap();
+        let scale_factor = primary_monitor.scale_factor();
+
+        // Desired window size in logical units
+        let logical_width = f64::from(app_params.window_width) / scale_factor;
+        let logical_height = f64::from(app_params.window_height) / scale_factor;
+        let window_size = LogicalSize::new(logical_width, logical_height);
+
+        // Monitor size in physical pixels
+        let monitor_size: PhysicalSize<u32> = primary_monitor.size();
+
+        // Calculate the top-left position to center the window
+        let monitor_width = monitor_size.width as f64;
+        let monitor_height = monitor_size.height as f64;
+        let pos_x = (monitor_width - logical_width * scale_factor) / 2.0;
+        let pos_y = (monitor_height - logical_height * scale_factor) / 2.0;
+        let position = Position::new(Position::Physical((pos_x, pos_y).into()));
+
+        let window = WindowBuilder::new()
             .with_title(&app_params.name)
-            .with_inner_size(winit::dpi::LogicalSize::new(
-                f64::from(app_params.window_width),
-                f64::from(app_params.window_height),
-            ))
+            .with_resizable(false)
+            .with_inner_size(window_size)
+            .with_position(position)
             .build(event_loop)
-            .unwrap_or_else(|err| panic!("Failed to initialize the window: {:?}\n", err))
+            .unwrap_or_else(|err| panic!("Failed to initialize the window: {:?}\n", err));
+
+        window
     }
 }
